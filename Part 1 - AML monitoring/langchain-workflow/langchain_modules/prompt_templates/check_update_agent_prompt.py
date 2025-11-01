51cf46dcfa3841a7beeaad9b03c87e6f693e715f
wuikong <126385630+wuikong@users.noreply.github.com>
2025-11-01 14:33:27 +0800

    check update agent prompt

---
+++ Part 1 - AML monitoring/langchain-workflow/langchain_modules/prompt_templates/check_update_agent_prompt.py
@@ -0,0 +1,3 @@
+CHECK_UPDATE_AGENT_PROMPT = """
+Given the existing regulatory notice and a potentially updated version of the notice, both in pdf format, use a hash comparison to determine if the updated notice has changed.
+"""
